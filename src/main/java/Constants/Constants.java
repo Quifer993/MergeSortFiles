1afecf829d3574f607bf6be18739463f87386147
Andrew <a.zolotorevskii@g.nsu.ru>
2023-08-24 17:43:17 +0700

    Create necessary dir`s, if they not created

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String TXT_FORMAT = ".txt";
     public static final String DIRECTORY_WORKING_FILES = "inputFiles/";
     public static final String DIRECTORY_OUTPUT_FILES = "outputFiles/";
+    public static final String DIRECTORY_EMPTY_FILES = "src/main/resources/emptiesFiles";
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,18 @@ import func.Sorters.ISorter;
 import func.Sorters.SorterInt;
 import func.Sorters.SorterString;
 
+import java.io.File;
+
+import static Constants.Constants.*;
+
 public class Main {
     public static void main(String[] args) {
         try {
+            //If these dir`s not exists, create them
+            createDirectories(DIRECTORY_WORKING_FILES);
+            createDirectories(DIRECTORY_OUTPUT_FILES);
+            createDirectories(DIRECTORY_EMPTY_FILES);
+
             Parser parser = new Parser(args);
             ISorter sorter;
             if(parser.getTypeSort() == TypeSort.INTEGER_TYPE){
@@ -20,6 +29,13 @@ public class Main {
             sorter.mergeSort();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void createDirectories(String dir) {
+        File directory = new File(dir);
+        if (directory.mkdir()) {
+            System.out.println("Directory " + dir + " successfully created!");
         }
     }
 }
